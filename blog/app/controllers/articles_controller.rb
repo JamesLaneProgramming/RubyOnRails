90a275508daf972b464bcc7759ccaa9441d6e396
JamesLaneProgramming <jamesjrlane@gmail.com>
2017-10-19 04:38:51 +0100

    [Added Authorisation for Actions]

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
-	http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
-	http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+	http_basic_authenticate_with name: "Lily", password: "", except: [:index, :show]
+	http_basic_authenticate_with name: "Lily", password: "", only: :destroy
 
 	def index
 	 @articles = Article.all
