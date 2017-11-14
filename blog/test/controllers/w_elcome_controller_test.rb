9d4b14c5c295e611872ee4e3e2d5184f59fbaf49
JamesLaneProgramming <jamesjrlane@gmail.com>
2017-11-14 14:55:09 +1100

    [Added devise Rego]

--- blog/test/controllers/w_elcome_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class WElcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get w_elcome_index_url
-    assert_response :success
-  end
-
-end
